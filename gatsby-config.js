bb99d6d52ab6711bfb51b26f39661a348351ebd7
Claudio Rojas <cerojasr@falabella.cl>
2021-03-11 16:58:48 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: "Tia Pikachu",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
