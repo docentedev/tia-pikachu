2bfda1bbbb81107a18cafd9c57eeb5d87ddadaf1
Claudio Rojas <cerojasr@falabella.cl>
2021-03-12 19:07:32 -0300

    Updates

---
+++ component---src-pages-404-js-5cc220c992f3ad7387be.js
@@ -0,0 +1,2 @@
+(self.webpackChunktia_pikachu=self.webpackChunktia_pikachu||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t);var a=n(7294),i=n(5444),o={color:"#232129",padding:"96px",fontFamily:"-apple-system, Roboto, sans-serif, serif"},r={marginTop:0,marginBottom:64,maxWidth:320};t.default=function(){return a.createElement("main",{style:o},a.createElement("title",null,"No encontrado"),a.createElement("h1",{style:r},"Página no encontrada"),a.createElement(i.Link,{to:"/"},"Inicio"),".")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-5cc220c992f3ad7387be.js.map
